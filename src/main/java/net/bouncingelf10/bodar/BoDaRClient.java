171cf394d2abd224f97d8ec4f9f2613a4c839c5c
BouncingElf10 <mathisbonne09@gmail.com>
2024-06-08 23:14:57 +0200

    Initial commit

---
+++ src/main/java/net/bouncingelf10/bodar/BoDaRClient.java
@@ -0,0 +1,10 @@
+package net.bouncingelf10.bodar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BoDaRClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
